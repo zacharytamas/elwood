be4605d0e4bd7c8b138fc0319bc018cb7ba96f47
Zachary Jones <zachary@jones.cx>
2020-03-20 14:21:20 -0400

    ♻️ Remove old message package
    This was from a very early version of this idea.

--- packages/message/README.md
+++
@@ -1,3 +0,0 @@
-# `@elwood/message`
-
-A package defining Messages and how to work with them.
--- packages/message/src/message/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './interfaces';
-export * from './parseRawMessage';
-export * from './dumpMessage';
