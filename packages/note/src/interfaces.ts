7af1c7de563ced5926e9c66ff76119846d865770
Zachary Jones <zachary@jones.cx>
2020-03-20 12:18:21 -0400

    [note] ✨ Add Informable interface

--- packages/note/src/interfaces.ts
+++ packages/note/src/interfaces.ts
@@ -7,7 +7,18 @@ export interface Reviewable {
   reviewedAt?: Date;
 }
 
-export type BaseNoteMeta = Auditable & Reviewable;
+/**
+ * Properties that describe the relationship between Notes and how their information
+ * informs or is informed by other Notes' information.
+ */
+export interface Informable {
+  /** A list of IDs of other Notes whose information is informed by this one's. */
+  informs?: NoteId[];
+  /** A list of IDs of other Notes whose information informs this one's. */
+  informedBy?: NoteId[];
+}
+
+export type BaseNoteMeta = Auditable & Reviewable & Informable;
 
 export interface Note<T extends BaseNoteMeta = BaseNoteMeta> {
   body: string;
